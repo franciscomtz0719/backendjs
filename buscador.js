e9364905ec9483a9c21fdb7342ff3317cf1f66f5
franciscomtz0719 <franciscomtz0719@gmail.com>
2022-06-29 19:22:24 -0500

    file system homework done

--- buscador.js
+++ buscador.js
@@ -1,4 +1,38 @@
 
+// const nombres = [
+//     "Paco",
+//     "Lorena",
+//     "Kike",
+//     "Abraham",
+//     "Cris",
+//     "Victor",
+//     "Marco",
+//     "Israel",
+//     "Annie",
+//     "Ërnesto",
+//     "Francisco",
+//     "Lenny",
+//     "Rod",
+//     "Noemi",
+//     "Benjamin",
+// ];
+
+// let strNombre= ""
+
+//     if (process.argv[2] !== undefined ){
+//         console.log(process.argv)
+//         strNombre = process.argv[2]
+// }
+
+// const buscador = nombres.filter(nombres => nombres === strNombre)
+
+// //verifica que el arreglo no este vacio
+// if (buscador.length >= 1){
+//     console.log (`Bienvenido ${buscador}`)
+// }else{
+//     console.log (`no existe`)
+// }
+
 const nombres = [
     "Paco",
     "Lorena",
@@ -9,25 +43,18 @@ const nombres = [
     "Marco",
     "Israel",
     "Annie",
-    "Ërnesto",
+    "Ernesto",
     "Francisco",
     "Lenny",
     "Rod",
     "Noemi",
     "Benjamin",
-];
+  ]
+  
+  const nombresLength = nombres.reduce((acc, value) => {
+  
+    return {...acc, [value]: value.length}
+  }, {});
+  
+  console.log(nombresLength)
 
-let strNombre= ""
-
-    if (process.argv[2] !== undefined ){
-        console.log(process.argv)
-        strNombre = process.argv[2]
-}
-
-const buscador = nombres.filter(nombres => nombres === strNombre)
-
-if (buscador.length >= 1){
-    console.log (`Bienvenido ${buscador}`)
-}else{
-    console.log (`no existe`)
-}
--- index.js
+++ index.js
@@ -1,46 +1,89 @@
-/**
- * hacer una funcion que reciba como parametro un nombre e imprime, Hola {nombre}, como estas?
- * 
- */
+// /**
+//  * hacer una funcion que reciba como parametro un nombre e imprime, Hola {nombre}, como estas?
+//  * 
+//  */
 
-// let nombre = 'Francisco'
+// // let nombre = 'Francisco'
 
-// const saludo = (() => {
-//     return `Hola ${nombre} como estas?`
-// })
+// // const saludo = (() => {
+// //     return `Hola ${nombre} como estas?`
+// // })
 
-// console.log(saludo(nombre))
+// // console.log(saludo(nombre))
 
-//arreglo con < 5 letras y otro con > de 5 letras 
-const nombres = [
-    "paco",
-    "Lorena",
-    "Kike",
-    "Abraham",
-    "Cris",
-    "Victor",
-    "Israel",
-    "Annie"
-]
+// //arreglo con < 5 letras y otro con > de 5 letras 
+// const nombres = [
+//     "paco",
+//     "Lorena",
+//     "Kike",
+//     "Abraham",
+//     "Cris",
+//     "Victor",
+//     "Israel",
+//     "Annie"
+// ]
 
-let lessThan = []
-let moreThan= []
+// let lessThan = []
+// let moreThan= []
 
-const mostrar = nombres.forEach((arr) => {
+// const mostrar = nombres.forEach((arr) => {
 
-    if (arr.length  < 5) {
+//     if (arr.length  < 5) {
             
-        lessThan.push(arr)
+//         lessThan.push(arr)
 
-    } else if (arr.length > 5) {
+//     } else if (arr.length > 5) {
 
-        moreThan.push(arr)
-    }
+//         moreThan.push(arr)
+//     }
 
 
     
-})
+// })
 
-console.log(lessThan)
-console.log(moreThan)
-// mostrar(nombres)
+// console.log(lessThan)
+// console.log(moreThan)
+// // mostrar(nombres)
+
+// let colors = require('colors')
+
+// console.log( `Hola me llamo ${process.argv[2]}`.rainbow)
+
+const fs = require('fs')
+
+// fs.readFile('hola.txt', 'utf8', (err, data) => {
+
+//     if (err) throw err
+//     console.log (data)
+
+// })
+
+// fs.writeFile('fsMkdirExample/ejemplo3.txt', 'contenido de ejemplo', 'utf8',(err) => {
+//     if (err) {
+//         console.log (`hay un error ${err}`)
+//         return
+//     }
+// })
+
+function deleteErr (err) {
+
+    if (err) {
+        console.log (`hay un error ${err}`)
+        return
+}
+        console.log ('borrado con exito')
+
+}
+
+// fs.unlink('ejemplo.txt', deleteErr)
+
+// function createDirErr (err) {
+
+//     if (err) {
+//         console.log (`hay un error ${err}`)
+//         return
+// }
+//         console.log ('Directorio creado con exito')
+
+// fs.rmdir('fsMkdirExample2', deleteErr )
+fs.rmdir('fsMkdirExample', {recursive: true} , deleteErr)
